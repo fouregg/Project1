f57462491a61c25cf406dff1a7e2fa3f5c450eef
alexey <aleksshow200410@gmail.com>
2024-07-19 20:14:45 +0300

    сделал показ изображений

--- scriptHM13.js
+++ scriptHM13.js
@@ -9,27 +9,28 @@ function artResponse() {
       const artworkId = data.data[j].id;
       const linkImg = data.data[j].api_link;
       const cardTitleText = data.data[j].title;
-      console.log(artworkId); // для проверки
-      console.log(cardTitleText); // для проверки
-      console.log(linkImg); // для проверки
-      // Создание IIIF URL - советует GPT
-      const URL_IMAGE =
-        data.config.iiif_url + "/" + imageId + "/full/843,/0/default.jpg";
-      // row
+
+      let imgLinkResult;
+
+      $.getJSON(linkImg, (responce) => {
+        console.log(responce)
+        imgLinkResult = `https://www.artic.edu/iiif/2/${responce.data.image_id}/full/1000,/0/default.jpg`;
+
+        // row
       let row = document.createElement("div");
       $(row).addClass("row mt-5 justify-content-around");
       document.body.getElementsByClassName("container")[0].appendChild(row);
       // card
       let card = document.createElement("div");
       $(card).addClass("card"); // add css class for element
-      card.style.width = "18rem"; // add style property width
+      card.style.width = "25rem"; // add style property width
       row.appendChild(card); // add element like child for element row
       // imag from GPT
       let imgElement = document.createElement("img");
       $(imgElement).addClass("card-img-top");
       card.appendChild(imgElement);
       // Установка src для изображения
-      imgElement.setAttribute("src", URL_IMAGE);
+      imgElement.setAttribute("src", imgLinkResult);
       // Дополнительный запрос для получения изображения
       //$.getJSON(imageURL, function (imageData) {
       //  imgElement.setAttribute("src", imageData.data.image.url);
@@ -43,6 +44,9 @@ function artResponse() {
       $(cardTitle).addClass("card-title");
       cardBody.appendChild(cardTitle);
       cardTitle.innerHTML = data.data[j].title; // title видит
+      });
+      
+      
     }
   });
 }
