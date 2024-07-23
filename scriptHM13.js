1ab42a8ad3ddf2cbff8a66f6c032e2ae0843247c
Denis <dzaveryaev@yandex.ru>
2024-07-23 08:45:42 +0300

    Update scriptHM13.js
    Added author into Card

--- scriptHM13.js
+++ scriptHM13.js
@@ -1,8 +1,8 @@
 function artResponse() {
   const URL_API = "https://api.artic.edu/api/v1/artworks/search?q=cats";
   const N = 10; // кол-во карточек
-  let responce = $.getJSON(URL_API, (data) => {
-    console.log(responce);
+  let response = $.getJSON(URL_API, (data) => {
+    console.log(response);
     console.log(data);
     for (let j = 0; j < N; j++) {
       if (typeof data.data[j] == "undefined") break;
@@ -15,33 +15,41 @@ function artResponse() {
       console.log(cardTitleText);
       // получение данных по каждой картинке, второй запрос
       let imgLinkResult;
-      $.getJSON(linkImg, (responce) => {
-        console.log(responce);
-        imgLinkResult = `https://www.artic.edu/iiif/2/${responce.data.image_id}/full/1000,/0/default.jpg`;
-        // row
+      let dispText;
+      $.getJSON(linkImg, (imgResponse) => {
+        console.log(imgResponse);
+        imgLinkResult = `https://www.artic.edu/iiif/2/${imgResponse.data.image_id}/full/1000,/0/default.jpg`;
         let row = document.createElement("div");
         $(row).addClass("row mt-5 justify-content-around");
         document.body.getElementsByClassName("container")[0].appendChild(row);
+
         // card
         let card = document.createElement("div");
         $(card).addClass("card"); // add css class for element
         card.style.width = "25rem"; // add style property width
         row.appendChild(card); // add element like child for element row
+
         // image
         let imgElement = document.createElement("img");
         $(imgElement).addClass("card-img-top");
         card.appendChild(imgElement);
+
         // Установка src для изображения
         imgElement.setAttribute("src", imgLinkResult);
         // cardBody
         let cardBody = document.createElement("div");
         $(cardBody).addClass("card-body");
         card.appendChild(cardBody);
-        // name -
+        // Card Title
         let cardTitle = document.createElement("h5");
         $(cardTitle).addClass("card-title");
         cardBody.appendChild(cardTitle);
         cardTitle.innerHTML = data.data[j].title;
+
+        // Добавляем информацию об авторе
+        let artistInfo = document.createElement("p");
+        artistInfo.innerHTML = imgResponse.data.artist_display;
+        cardBody.appendChild(artistInfo);
       });
     }
   });
