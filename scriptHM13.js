606e2a3cf2e10d63e1b6487b63bc360946e0ca41
Denis <dzaveryaev@yandex.ru>
2024-07-23 08:48:06 +0300

    Update scriptHM13.js

--- scriptHM13.js
+++ scriptHM13.js
@@ -10,15 +10,26 @@ function artResponse() {
       const artworkId = data.data[j].id;
       const linkImg = data.data[j].api_link;
       const cardTitleText = data.data[j].title;
-      console.log(artworkId);
-      console.log(linkImg);
-      console.log(cardTitleText);
+      console.log(`ID изображения artworkId: ${artworkId}`);
+      console.log(`Link Image: ${linkImg}`);
+      console.log(`Title : ${cardTitleText}`);
       // получение данных по каждой картинке, второй запрос
       let imgLinkResult;
+<<<<<<< Updated upstream
       let dispText;
       $.getJSON(linkImg, (imgResponse) => {
         console.log(imgResponse);
         imgLinkResult = `https://www.artic.edu/iiif/2/${imgResponse.data.image_id}/full/1000,/0/default.jpg`;
+=======
+      let otherInfo;
+      $.getJSON(linkImg, (responce) => {
+        console.log(responce);
+        imgLinkResult = `https://www.artic.edu/iiif/2/${responce.data.image_id}/full/1000,/0/default.jpg`;
+        otherInfo = `https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number`;
+        //const cardText = data.data[j].date_display;
+        //console.log(cardText);
+        // row
+>>>>>>> Stashed changes
         let row = document.createElement("div");
         $(row).addClass("row mt-5 justify-content-around");
         document.body.getElementsByClassName("container")[0].appendChild(row);
@@ -45,11 +56,19 @@ function artResponse() {
         $(cardTitle).addClass("card-title");
         cardBody.appendChild(cardTitle);
         cardTitle.innerHTML = data.data[j].title;
+<<<<<<< Updated upstream
 
         // Добавляем информацию об авторе
         let artistInfo = document.createElement("p");
         artistInfo.innerHTML = imgResponse.data.artist_display;
         cardBody.appendChild(artistInfo);
+=======
+        // card-text
+        let cardText = document.createElement("h5");
+        $(cardText).addClass("card-text");
+        cardBody.appendChild(cardText);
+        cardText.innerHTML = data.data[j].artist_display;
+>>>>>>> Stashed changes
       });
     }
   });
