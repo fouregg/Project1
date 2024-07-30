638e9c4c0782ee307470ba8b96c0e666d7f4745c
Denis <dzaveryaev@yandex.ru>
2024-07-30 23:25:03 +0300

    forms

---
+++ form1.php
@@ -0,0 +1,7 @@
+<?php
+$_email = htmlspecialchars($_POST['email']);
+$_password = htmlspecialchars($_POST['password']);
+$_firstName = htmlspecialchars($_POST['firstName']);
+$_secondName = htmlspecialchars($_POST['lastName']);
+$_city = htmlspecialchars($_POST['city']);
+$_gender = htmlspecialchars($_POST['gender']);
